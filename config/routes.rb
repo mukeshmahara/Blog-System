a8a5fdd90d2569ed4fb489c714ee761c66bc5dbe
Mukesh Mahara <mukeshmahara1@gmail.com>
2021-06-15 22:42:44 +0545

    configurtion folder

---
+++ config/routes.rb
@@ -0,0 +1,16 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # root to:'user#index'
+
+
+  resources :blog
+  resources :user
+  # resources :login
+  get "/login", to: "login#index"
+  post "/login", to: "login#check_credentials"
+  # namespace :user do
+  #   resources :blog
+  # end
+
+end
