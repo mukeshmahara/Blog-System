0a3aca2a9a875a2ece7f04a5368f66c310bbc216
Mukesh Mahara <mukeshmahara1@gmail.com>
2021-06-16 10:45:24 +0545

    added authentication feature

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -2,34 +2,61 @@ class LoginController < ApplicationController
 
 def index
 
-    
+   
 end
 
 def check_credentials
-    # @user = User.find_by(username: login_params[:username])
-    # puts @user
 
-    @uname = User.pluck(:username)
-    @pass = User.pluck(:password)
-    @username = params[:username]
-    @password = params[:password]
-    puts @uname
-    puts "ok"
-    puts @password
-    # if @uname.include? username &&  @pass.include? password
-    #     session[:user_id] = user_id
-    #     redirect_to "blog_index_path"
-    # else
-    #     flash[:login_errors] = ["Invalid credentials"]
-    #     redirect_to "/login"
-    # end
+    
+    # @user = User.authenticate(params[:username], params[:password])
+    # puts @user
+    # @user = User.find_by(:username => params[:username])
+    # @user = User.create(params.require(:user).permit(:username,        
+    #     :password))  
+    # @user = User.find_by(:id)  
+    # puts @user.id
+    
+    
+
+    @uname = User.pluck(:username).to_a
+    @pass = User.pluck(:password).to_a
+    username = params[:username]
+    password = params[:password]
+
+    @user = User.find_by(:username => username)  
+    puts @user
+    
+    if username =="test" && password == "test"
+        
+        session[:user_id] = username
+        # session[:user_id]
+        redirect_to "/blog"
+    else
+        flash[:login_errors] = ["Invalid credentials"]
+        redirect_to "/login"
+    end
+
+    
+
+end
+def logout
+    # session[:user_id].
+    reset_session  
+
+    if session[:user_id].nil?
+        flash[:logout_msg] = ["Successfully logout"]
+        
+    end
+    redirect_to "/login" 
+
+    
 end
 
-# private 
-#     def login_params
-#         params.permit(:username, :password)
+private 
+    def login_params
+        params.require(:login).permit(:username, :password)
             
-#     end
+    end
 
 
 
