e88fdf7f366fa37ec22591ad0c54d9a7a6665c8d
Mukesh Mahara <mukeshmahara1@gmail.com>
2021-06-27 13:55:33 +0545

    profile controller

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -4,6 +4,12 @@ def show
     username = request.path_parameters.values
     @user = User.find_by(:username => username[2])
 
+
+
+    @blog = Blog.where(:blog_author => username[2])
+
+    
+
     
 end
 
