7b6a5cda490f7ec2e1098e792ca0ea7d0e018fb2
Mukesh Mahara <mukeshmahara1@gmail.com>
2021-07-03 16:37:03 +0545

    added routes for comment

--- config/routes.rb
+++ config/routes.rb
@@ -3,18 +3,21 @@ Rails.application.routes.draw do
 
   root to:'blog#index'
 
-
-  resources :blog
-  resources :user
-  resources :profile
-
-  resources :register
+  resources :blog, :user, :profile, :register
   
+  # resources :blog do
+  #   resources :comment, only: [:index, :new, :create]
+  # end
+
   get "/user/profile", to: "user#index"
   
   get "/logout", to: "login#logout"
   get "/login", to: "login#index"
   post "/login", to: "login#check_credentials"
+  # post "/blog", to: "blog#create_comment"
+
+
+
 
   get "/search", to: "blog#blogSearch"
   
