7edc6af1e3badd2155edced13bfc9507849b706f
Mukesh Mahara <mukeshmahara1@gmail.com>
2021-07-06 07:58:55 +0545

    added method for getting comment on specific blog

--- app/helpers/blog_helper.rb
+++ app/helpers/blog_helper.rb
@@ -1,2 +1,10 @@
 module BlogHelper
+    def comment_on_specific_blog(id)
+        @comments= Comment.where(:blog_id=>id)
+        
+        puts "TESTING"
+        puts @comments.first.inspect
+        puts "TESTING"
+        return @comments
+      end
 end
