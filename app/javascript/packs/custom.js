a1b74b2dcf22b0a8f8eb1594342d2eb7e8b198f9
Mukesh Mahara <mukeshmahara1@gmail.com>
2021-07-06 15:52:03 +0545

    added comment functionality to all the blogs

--- app/javascript/packs/custom.js
+++ app/javascript/packs/custom.js
@@ -5,46 +5,44 @@ document.addEventListener("turbolinks:load", function() {
     let toggleLIGHT = document.getElementById("toggle-label-light");
 
     themeToggler.onclick = () => {
-    if (themeToggler.checked == true) {
-        document.body.classList.add("bg-dark");
-        toggleDark.classList.add("d-none");
-        toggleLIGHT.classList.add("d-block");
-        document.getElementById("box").classList.add("bg-dark");
-        document.getElementById("box").classList.add("text-light");
-        
-    } else {
-        document.body.classList.remove("bg-dark");
-        toggleDark.classList.remove("d-none");
-        toggleLIGHT.classList.remove("d-block");
-        document.getElementById("box").classList.remove("bg-dark");
-        document.getElementById("box").classList.remove("text-light");
+        if (themeToggler.checked == true) {
+            document.body.classList.add("bg-dark");
+            toggleDark.classList.add("d-none");
+            toggleLIGHT.classList.add("d-block");
+            document.getElementById("box").classList.add("bg-dark");
+            document.getElementById("box").classList.add("text-light");
+            
+        } else {
+            document.body.classList.remove("bg-dark");
+            toggleDark.classList.remove("d-none");
+            toggleLIGHT.classList.remove("d-block");
+            document.getElementById("box").classList.remove("bg-dark");
+            document.getElementById("box").classList.remove("text-light");
 
-    }
+        }
     };
 
+    var blog_id =  ($('#meroId').data('id'))
+    console.log(blog_id)
+
+    likebtn = document.getElementById("likebtn"+blog_id)
+    console.log(likebtn)
+    commentbtn = document.getElementById("cmntbtn"+blog_id)
+    console.log(commentbtn)
+
+    sharebtn = document.getElementById("sharebtn"+blog_id)
+    console.log(sharebtn)
+    var count = 1;
     
-    likebtn = document.getElementById("likebtn")
-    commentbtn = document.getElementById("cmntbtn")
-    sharebtn = document.getElementById("sharebtn")
-    var count = 0;
     
     likebtn.onclick=()=>{
         document.getElementById("likes").innerHTML = count++;
+        
+    }
+    commentbtn.onclick=()=>{
+        document.getElementById('comment'+blog_id).focus()
 
     }
 
-    state = false
-
-    commentbtn.addEventListener('click',(event)=>{
-    
-    document.getElementById('comment_box').classList.remove("d-none")  
-    
-    document.getElementById('comment_box').classList.add("d-block")  
-    })
-
-
-
-
-    
 });
 
