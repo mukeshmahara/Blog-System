6e2a64e3b9232fce9344da2a207fa3156ae7b2a7
Mukesh Mahara <mukeshmahara1@gmail.com>
2021-07-07 15:07:37 +0545

    define method for deleting the comment on the blog

--- app/helpers/blog_helper.rb
+++ app/helpers/blog_helper.rb
@@ -1,10 +1,25 @@
 module BlogHelper
-    def comment_on_specific_blog(id)
-        @comments= Comment.where(:blog_id=>id)
+    def comment_on_specific_blog(blog_id)
+        @comments= Comment.where(:blog_id=>blog_id).order("created_at DESC")
         
-        puts "TESTING"
-        puts @comments.first.inspect
-        puts "TESTING"
         return @comments
+      end
+
+      def find_UserbyCommentID(user_id)
+        @user = User.where(:id=>user_id)
+        return @user.first;
+      end
+
+
+      def delete_comment(comment_id)
+      
+        @comment = Comment.where(:id=>comment_id)
+      
+      end
+
+      def edit_comment(comment_id,new_comment)
+      
+        @comment = Comment.update(:id=>comment_id, :comment=>new_comment)
+      
       end
 end
