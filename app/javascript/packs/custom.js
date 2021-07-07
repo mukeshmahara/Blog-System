55ba5165f3587538d286b8938de1d9baef5e5591
Mukesh Mahara <mukeshmahara1@gmail.com>
2021-07-07 15:06:45 +0545

    added toggle logic for more option in comments

--- app/javascript/packs/custom.js
+++ app/javascript/packs/custom.js
@@ -42,10 +42,39 @@ document.addEventListener("turbolinks:load", function() {
     var count1=0;
     // console.log(document.querySelectorAll('.cmntbutton'))
 
+    document.querySelectorAll('.show-more-comment').forEach(element=>{
+        element.addEventListener('click',(event)=>{
+            const id = event.target.id
+            document.getElementById('comment-section'+id).classList.remove('d-none')
+            document.getElementById('comment'+id).focus()
+
+        })
+    })
+
+    var count2=0
+    document.querySelectorAll('.option').forEach(element=>{
+        var id =document.querySelectorAll('.option>.show')[count2++].id.split("more-option")[1]
+        
+        
+        $('#more-option'+id).hide(); 
+
+        $('#more-option-parent'+id).mouseover(function () {
+            $('#more-option'+id).show();      
+        });
+        $('#more-option-parent'+id).mouseout(function () {
+            $('#more-option'+id).hide();      
+        });
+
+        
+    })
+
+    
+
     document.querySelectorAll('.cmntbutton').forEach(element =>{
         var id =document.querySelectorAll('.cmntbutton')[count1++].id.split("cmntbtn")[1]
         element.addEventListener('click',(event)=>{
             // const id = event.target.id
+            document.getElementById('comment-section'+id).classList.remove('d-none')
             document.getElementById('comment'+id).focus()
         })
     })
