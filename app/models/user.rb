8316e27bc73d87ef81a2e6ef703ad630501190bb
Mukesh Mahara <mukeshmahara1@gmail.com>
2021-07-07 17:22:39 +0545

    minor change

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
     # devise :database_authenticatable, :recoverabe, :rememberable
     has_many :blogs
     has_one_attached :avatar
+    
     # validates :username, :email, :password, :job, presence:true
     
 end
