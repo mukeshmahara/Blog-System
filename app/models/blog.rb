f9a3d23e8be2a16e7713471abc96104a6834b2ef
Mukesh Mahara <mukeshmahara1@gmail.com>
2021-07-07 17:17:40 +0545

    fixed issue of deleting record of chaild class when deleteing record of parent class

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,5 +1,6 @@
 class Blog < ApplicationRecord
     belongs_to :user, foreign_key: :user_id
+    has_many :comments, dependent: :destroy
     has_many_attached :images
     has_rich_text :content
     # validates :blog_title, :blog_author, presence:true
