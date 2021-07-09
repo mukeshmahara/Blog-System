80ecf8eccf3e8fdc1d20cad63aa2e210bd967377
Mukesh Mahara <mukeshmahara1@gmail.com>
2021-07-09 07:18:10 +0545

    purging trix in production mode

--- postcss.config.js
+++ postcss.config.js
@@ -1,4 +1,4 @@
-module.exports = {
+  module.exports = {
   plugins: [
     require('postcss-import'),
     require('postcss-flexbugs-fixes'),
@@ -9,4 +9,20 @@ module.exports = {
       stage: 3
     })
   ]
+}
+
+if (process.env.RAILS_ENV === "production") {
+  environment.plugins.push(
+    require('@fullhuman/postcss-purgecss')({
+      content: [
+        './app/**/*.html.erb',
+        './app/helpers/**/*.rb',
+        './app/components/**/*.rb',
+        './app/javascript/**/*.js',
+        
+      ],
+      whitelistPatterns: [/trix-*/],
+      defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
+    })
+  )
 }
