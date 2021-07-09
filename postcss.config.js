544e3968f0a50fb7f7e3a90b36bdb0cdac79265c
Mukesh Mahara <mukeshmahara1@gmail.com>
2021-07-09 09:09:01 +0545

    test 2

--- postcss.config.js
+++ postcss.config.js
@@ -10,19 +10,3 @@
     })
   ]
 }
-
-if (process.env.RAILS_ENV === "production") {
-  environment.plugins.push(
-    require('@fullhuman/postcss-purgecss')({
-      content: [
-        './app/**/*.html.erb',
-        './app/helpers/**/*.rb',
-        './app/components/**/*.rb',
-        './app/javascript/**/*.js',
-        
-      ],
-      whitelistPatterns: [/trix-*/],
-      defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
-    })
-  )
-}
