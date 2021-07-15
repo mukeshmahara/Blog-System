6e3a854d5b926349dbe445d3108d6b99bba5e033
Mukesh Mahara <mukeshmahara1@gmail.com>
2021-07-15 21:25:34 +0545

    registerController

--- app/controllers/register_controller.rb
+++ app/controllers/register_controller.rb
@@ -1,13 +1,16 @@
 class RegisterController < ApplicationController
 
     def create
-    
+       
         @user = User.new(params.require(:register).permit(:username, :email, :password,:gender,:fb_link,:insta_link,:age,:twitter_link,:stack_overflow_link,:address,:job, :phone_number,:avatar ))
+        puts @user.valid?
         if(@user.save)
+
             flash[:user_created_msg] = ["User has been created successfully"]
             redirect_to "/login"
         else
-            flash[:user_creation_failure_msg] = ["Some thing went wrong"]
+            
+            # flash[:user_creation_failure_msg] = ["Some thing went wrong"]
             redirect_to "/register"
         end
     end
